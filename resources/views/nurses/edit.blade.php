59395f44a264f0e55296c0dbdc504a7c8adc91bf
echuquillanquiy <edward.axl3@gmail.com>
2023-03-16 09:33:39 -0500

    corrigiendo llenado de horas tto enfermeria

--- resources/views/nurses/edit.blade.php
+++ resources/views/nurses/edit.blade.php
@@ -218,7 +218,7 @@
                     @if($nurse->nhd)
                         <input type="text" name="nhd" class="form-control" value="{{ $nurse->nhd }}">
                     @else
-                        <input type="text" name="nhd" class="form-control" value="{{ old('nhd', $ult) }}">
+                        <input type="text" name="nhd" class="form-control" value="{{ old('nhd', $nurse->nhd) }}">
                     @endif
                 </div>
 
