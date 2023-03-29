edeb658b4484112e92868e36873aa78365beb1ed
echuquillanquiy <edward.axl3@gmail.com>
2023-03-29 16:03:28 -0500

    condicionando medicamentos

--- app/Http/Controllers/Doctor/MedicalController.php
+++ app/Http/Controllers/Doctor/MedicalController.php
@@ -199,10 +199,10 @@ class MedicalController extends Controller
         $ultimos = $medical->where('patient', $patient)->whereDate('created_at', '!=', $fecha)->latest()->first();
 
         $medical->epo = $medical->epo ? $medical->epo : $ultimos->epo;
-        $medical->epo4000 = $medical->epo4000 ? $medical->epo4000 :  $ultimos->epo4000;
-        $medical->iron = $medical->iron ? $medical->iron :  $ultimos->iron;
-        $medical->vitb12 = $medical->vitb12 ? $medical->vitb12 :  $ultimos->vitb12;
-        $medical->calci = $medical->calci ? $medical->calci :  $ultimos->calci;
+        $medical->epo4000 = $medical->epo4000 ? $medical->epo4000 : $ultimos->epo4000;
+        $medical->iron = $medical->iron ? $medical->iron : $ultimos->iron;
+        $medical->vitb12 = $medical->vitb12 ? $medical->vitb12 : $ultimos->vitb12;
+        $medical->calci = $medical->calci ? $medical->calci : $ultimos->calci;
 
         return view('medicals.edit', compact('medical'));
 
