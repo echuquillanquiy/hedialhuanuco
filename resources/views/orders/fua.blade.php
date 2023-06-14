8c16e7982a5d1e5581b22d53487b91aca896dbd7
echuquillanquiy <edward.axl3@gmail.com>
2023-06-14 13:01:10 -0500

    terminando anverso

--- resources/views/orders/fua.blade.php
+++ resources/views/orders/fua.blade.php
@@ -1,5 +1,5 @@
-<div style="font-family: Arial,sans-serif; text-transform: uppercase;">
-    <table style="margin-top: -40px; margin-left: -20px;width: 100%">
+<div style="font-family: Arial,sans-serif; text-transform: uppercase; margin: 0px auto;width: 100%; border: black 2px solid; padding: 5px">
+    <table >
 
         <tr>
             <td colspan="10" style="text-align: left">
@@ -28,17 +28,17 @@
     </table>
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
-        <tr style="border: #0a0c0d solid 1px; background: #A8A8A8; font-size: 0.6rem; font-weight: bold">
-            <td>CODIGO IPRESS</td>
-            <td>NOMBRE DE IPRESS QUE REALIZA LA ATENCION</td>
-            <td>SUBSANACION</td>
+        <tr style="border: #0a0c0d solid 1px; background: #A8A8A8; font-size: 0.5rem; font-weight: bold">
+            <td style="height: 15px; width: 150px">CODIGO IPRESS</td>
+            <td style="width: 300px">NOMBRE DE IPRESS QUE REALIZA LA ATENCION</td>
+            <td style="width: 180px;">SUBSANACION</td>
             <td style="background: white; padding: 2px"></td>
         </tr>
 
         <tr>
-            <td rowspan="2" style="font-size: 0.7rem;">8181</td>
-            <td rowspan="2" style="font-size: 0.7rem;">H&S OCCUPATIONAL SAC</td>
-            <td colspan="2" style="font-size: 0.5rem;">N° FORMATO ATENCION PARA SUBSANACION</td>
+            <td rowspan="2" style="font-size: 0.7rem; font-weight: bold">8181</td>
+            <td rowspan="2" style="font-size: 0.7rem; font-weight: bold">H&S OCCUPATIONAL SAC</td>
+            <td colspan="2" style="font-size: 0.5rem; font-weight: bold">N° FORMATO ATENCION PARA SUBSANACION</td>
         </tr>
 
         <tr>
@@ -50,17 +50,17 @@
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
         <tr>
-            <td colspan="3" style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8;">FECHA DE ATENCION</td>
-            <td style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">HORA</td>
-            <td style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">N° HISTORIA CLINICA</td>
-            <td colspan="4" style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">IDENTIFICACION</td>
-            <td colspan="2" style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">REGIMEN</td>
+            <td colspan="3" style="height: 15px; font-size: 0.5rem; font-weight: bold; background: #A8A8A8;">FECHA DE ATENCION</td>
+            <td style="height: 15px; font-size: 0.5rem; font-weight: bold; background: #A8A8A8">HORA</td>
+            <td style="height: 15px; font-size: 0.5rem; font-weight: bold; background: #A8A8A8">N° HISTORIA CLINICA</td>
+            <td colspan="4" style="height: 15px; font-size: 0.5rem; font-weight: bold; background: #A8A8A8">IDENTIFICACION</td>
+            <td colspan="2" style="height: 15px; font-size: 0.5rem; font-weight: bold; background: #A8A8A8">REGIMEN</td>
         </tr>
 
         <tr>
-            <td style="font-size: 0.6rem; background: #BEBEBE">DIA</td>
-            <td style="font-size: 0.6rem; background: #BEBEBE">MES</td>
-            <td style="font-size: 0.6rem; background: #BEBEBE">AÑO</td>
+            <td style="height: 20px; font-size: 0.6rem; background: #BEBEBE">DIA</td>
+            <td style="height: 20px; font-size: 0.6rem; background: #BEBEBE">MES</td>
+            <td style="height: 20px; font-size: 0.6rem; background: #BEBEBE">AÑO</td>
             <td rowspan="2" style="font-size: 0.7rem; font-weight: bold">{{ $order->created_at->format('G:i') }}</td>
             <td rowspan="2" style="font-size: 0.6rem; font-weight: bold">{{ $order->patient->dni }}</td>
             <td rowspan="2" style="font-size: 0.5rem; background: #BEBEBE">TD</td>
@@ -68,47 +68,47 @@
             <td rowspan="2" style="font-size: 0.5rem; background: #BEBEBE">N° DOCUMENTO</td>
             <td rowspan="2" style="font-size: 0.6rem; font-weight: bold">{{ $order->patient->dni }}</td>
             <td style="font-size: 0.4rem; background: #BEBEBE">SUBSIDIADO</td>
-            <td style="font-size: 0.5rem; font-weight: bold;"></td>
+            <td style="font-size: 0.5rem; font-weight: bold;">X</td>
         </tr>
 
         <tr>
-            <td style="font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('d') }}</td>
-            <td style="font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('m') }}</td>
-            <td style="font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('Y') }}</td>
+            <td style="height: 20px; font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('d') }}</td>
+            <td style="height: 20px; font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('m') }}</td>
+            <td style="height: 20px; font-size: 0.6rem;  font-weight: bold">{{ $order->created_at->format('Y') }}</td>
             <td style="font-size: 0.4rem; background: #BEBEBE">SEMICONTRIBUTIVO</td>
             <td style="font-size: 0.5rem; font-weight: bold;"></td>
         </tr>
     </table>
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
-        <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">
-            <td>APELLIDO PATERNO</td>
-            <td>APELLIDO MATERNO</td>
+        <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8;">
+            <td style="height: 20px">APELLIDO PATERNO</td>
+            <td style="height: 20px">APELLIDO MATERNO</td>
         </tr>
 
         <tr style="font-size: 0.6rem; font-weight: bold;">
-            <td style="padding: 5px">{{ $ap_pat }}</td>
-            <td style="padding: 5px">{{ $ap_mat }}</td>
+            <td style="height: 22px">{{ $ap_pat }}</td>
+            <td style="height: 22px">{{ $ap_mat }}</td>
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8;">
-            <td>PRIMER NOMBRE</td>
-            <td>OTROS NOMBRES</td>
+            <td  style="height: 20px">PRIMER NOMBRE</td>
+            <td  style="height: 20px">OTROS NOMBRES</td>
         </tr>
 
         <tr style="font-size: 0.6rem; font-weight: bold;">
-            <td style="padding: 5px">{{ $primeronom }}</td>
-            <td style="padding: 5px">{{ $otrosnom }}</td>
+            <td style="height: 22px">{{ $primeronom }}</td>
+            <td style="height: 22px">{{ $otrosnom }}</td>
         </tr>
     </table>
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
-        <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">
-            <td colspan="4">TIPO DE PRESTACION</td>
+        <tr style="font-size: 0.6rem; font-weight: bold; background: #A8A8A8">
+            <td colspan="4" style="height: 20px">TIPO DE PRESTACION</td>
         </tr>
 
         <tr>
-            <td style="font-size: 0.6rem; background: #BEBEBE; text-align: left"> Consulta externa</td>
+            <td style="font-size: 0.6rem; background: #BEBEBE; text-align: left; height: 20px"> Consulta externa</td>
             <td></td>
             <td style="font-size: 0.6rem; background: #BEBEBE; text-align: left"> Atencion de procedimientos ambulatorios</td>
             <td style="font-size: 0.6rem; font-weight: bold;">X</td>
@@ -116,28 +116,28 @@
     </table>
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
-        <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">
+        <tr style="font-size: 0.6rem; font-weight: bold; background: #A8A8A8">
             <td rowspan="2" style="width: 20px">N°</td>
-            <td colspan="5">DIAGNOSTICOS</td>
+            <td colspan="5" style="height: 20px">DIAGNOSTICOS</td>
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold; background: #BEBEBE">
-            <td style="width: 490px">DESCRIPCION</td>
+            <td style="width: 490px; height: 20px">DESCRIPCION</td>
             <td style="width: 90px">CIE - 10</td>
             <td colspan="3">TIPO Dx</td>
         </tr>
 
-        <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">1</td>
-            <td style="text-align: left">dfsff</td>
-            <td style="">N18.0</td>
+        <tr style="font-size: 0.6rem; font-weight: bold;">
+            <td style="height: 25px">1</td>
+            <td style="text-align: left">ENFERMEDAD RENAL CRONICA 5 EN HEMODIALISIS</td>
+            <td style="font-size: 0.6rem">N18.0</td>
             <td></td>
             <td>D</td>
             <td></td>
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">2</td>
+            <td style="height: 25px">2</td>
             <td style="text-align: left"></td>
             <td></td>
             <td>P</td>
@@ -146,7 +146,7 @@
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">3</td>
+            <td style="height: 25px">3</td>
             <td style="text-align: left"></td>
             <td></td>
             <td>P</td>
@@ -155,7 +155,7 @@
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">4</td>
+            <td style="height: 25px">4</td>
             <td style="text-align: left"></td>
             <td></td>
             <td>P</td>
@@ -164,7 +164,7 @@
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">5</td>
+            <td style="height: 25px">5</td>
             <td style="text-align: left"></td>
             <td></td>
             <td>P</td>
@@ -173,7 +173,7 @@
         </tr>
 
         <tr style="font-size: 0.5rem; font-weight: bold;">
-            <td style="height: 20px">6</td>
+            <td style="height: 25px">6</td>
             <td style="text-align: left"></td>
             <td></td>
             <td>P</td>
@@ -185,7 +185,7 @@
 
     <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;" border="1px">
         <tr style="font-size: 0.5rem; font-weight: bold; background: #A8A8A8">
-            <td style="width: 130px;">N° DNI</td>
+            <td style="width: 130px; height: 25px">N° DNI</td>
             <td> NOMBRE DEL RESPONSABLE DE LA ATENCION</td>
             <td style="width: 130px;">N° COLEGIATURA</td>
         </tr>
@@ -211,13 +211,13 @@
 
             <td style="border: white"></td>
 
-            <td rowspan="5" style="border: #000000 1px solid; width: 200px;">
-                <p></p>
+            <td rowspan="3" style="border: #000000 1px solid; width: 200px;">
+
             </td>
         </tr>
 
         <tr style="font-size: 0.4rem; text-align: left;">
-            <td colspan="5" style="border: black 1px solid">1= MÉDICO; 2=FARMACÉUTICO; 3=ODONTOLOGO; 4=BIÓLOGO; 5=OBSTETRIZ; 6= ENFERMERA;
+            <td colspan="5" style="border: black 1px solid; height: 25px">1= MÉDICO; 2=FARMACÉUTICO; 3=ODONTOLOGO; 4=BIÓLOGO; 5=OBSTETRIZ; 6= ENFERMERA;
                 7= TRABAJADORA SOCIAL; 8=PSICÓLOGO; 9=TECNÓLOGO MÉDICO;
                 10=NUTRICIONISTA; 11= TÉCNICO ENFERMERÍA; 12=AUXILIAR DE ENFERMERÍA
             </td>
@@ -226,30 +226,28 @@
         </tr>
 
         <tr>
-            <td style="height: 50px" colspan="5"></td>
+            <td style="height: 150px" colspan="5"></td>
         </tr>
 
+    </table>
 
-        <tr>
-            <td style="height: 50px"></td>
-        </tr>
-
-
+    <table width="100%" style="border-collapse:collapse;text-align:center; margin-top:5px;">
         <tr style="font-size: 0.4rem; font-weight: bold;">
 
-
             @if($order->medical->user_id)
-                <td colspan="3" style="text-align: center">
-                    <img src="{{ asset($order->medical->user->image) }}" style="width: 25%">
+                <td style="text-align: center">
+                    <img src="{{ asset($order->medical->user->image) }}" width="120px" height="50px">
                 </td>
             @else
-                <p>AQUI VA SELLO DEL MEDICO</p>
-                @endif
+                <td>
+                    Aqui va firma del medico
+                </td>
+            @endif
 
-            <td colspan="2">Firma del asegurado o apoderado</td>
+            <td>Firma del asegurado o apoderado</td>
+
+            <td>Huella digital del Asegurado o Apoderado</td>
         </tr>
-
-
     </table>
 
 </div>
