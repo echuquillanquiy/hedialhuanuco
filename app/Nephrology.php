430093490efb18302ec2bebd7eb81d409b4a8dc4
echuquillanquiy <edward.axl3@gmail.com>
2023-07-18 17:53:00 -0500

    seeders

--- app/Nephrology.php
+++ app/Nephrology.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nephrology extends Model
 {
-    protected $guarded = ['id', 'date_order'];
+    protected $guarded = ['id'];
 
     public function order()
     {
