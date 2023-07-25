1a52d79b6e235399eecd043c64880c895fc6c941
echuquillanquiy <edward.axl3@gmail.com>
2023-07-25 09:54:02 -0500

    seeders

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -85,13 +85,13 @@
             <i class="ni ni-curved-next text-dark"></i> Formato006
         </a>
     </li>
+        @if(auth()->user()->id == 1 && auth()->user()->id == 20)
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('medical.fissalweb') }}">
+                    <i class="ni ni-curved-next text-dark"></i> FISSAL
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('medical.fissalweb') }}">
-                <i class="ni ni-curved-next text-dark"></i> FISSAL
-            </a>
-        </li>
-        @if(auth()->user()->id == 1)
             <li class="nav-item">
                 <a class="nav-link" href="/nephrologies">
                     <i class="ni ni-curved-next text-dark"></i> CONSULTAS
