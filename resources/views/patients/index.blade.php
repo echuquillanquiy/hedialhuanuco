93b08b63c7a45d6b9360e190df8101daa77b2d90
echuquillanquiy <edward.axl3@gmail.com>
2024-09-05 16:28:59 -0500

    ultimos ajustes

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -69,7 +69,7 @@
         @foreach ($patients as $patient)
         <tr>
           <th scope="row">
-            {{$patient->firstname}} {{$patient->othername}} {{$patient->surname}} {{$patient->lastname}}
+              {{$patient->surname}} {{$patient->lastname}} {{$patient->firstname}} {{$patient->othername}}
           </th>
           <td>
             {{$patient->dni}}
