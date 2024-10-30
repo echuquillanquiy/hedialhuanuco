0ccfbf4eac45d4194af5d70808dfab89876f36d9
echuquillanquiy <edward.axl3@gmail.com>
2024-10-30 09:03:24 -0500

    ultimos ajustes

--- resources/views/medicals/edit.blade.php
+++ resources/views/medicals/edit.blade.php
@@ -62,7 +62,7 @@
 
                   <div class="form-group col-sm-12 col-lg-4">
                       <label for="clinical_trouble">Problemas Clínicos</label>
-                      <textarea class="form-control  @error('clinical_trouble') border border-danger @enderror" id="" name="clinical_trouble" rows="2">{{ !$medical->clinical_trouble ? optional($ultimaOrdenNoVacia)->clinical_trouble : $medical->clinical_trouble }}</textarea>
+                      <textarea class="form-control  @error('clinical_trouble') border border-danger @enderror" id="" name="clinical_trouble" rows="2">{{ old('clinical_trouble', !$medical->clinical_trouble ? optional($ultimaOrdenNoVacia)->clinical_trouble : $medical->clinical_trouble) }}</textarea>
                       @error('clinical_trouble')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -70,7 +70,7 @@
 
                       <div class="form-group col-sm-12 col-lg-4">
                           <label for="signal">Signos y Sintomas</label>
-                          <textarea class="form-control @error('signal') border border-danger @enderror" id="" name="signal" rows="2">{{ !$medical->signal ? optional($ultimaOrdenNoVacia)->signal : $medical->signal }}</textarea>
+                          <textarea class="form-control @error('signal') border border-danger @enderror" id="" name="signal" rows="2">{{ old('signal', !$medical->signal ? optional($ultimaOrdenNoVacia)->signal : $medical->signal) }}</textarea>
                           @error('signal')
                           <div class="text-danger text-center text-sm">{{ $message }}</div>
                           @enderror
@@ -78,7 +78,7 @@
 
               <div class="form-group col-sm-12 col-lg-4">
                   <label for="fua_observacion">Observaciones</label>
-                  <textarea class="form-control @error('fua_observacion') border border-danger @enderror" id="" name="fua_observacion" rows="2">{{ !$medical->fua_observacion ? optional($ultimaOrdenNoVacia)->fua_observacion : $medical->fua_observacion }}</textarea>
+                  <textarea class="form-control @error('fua_observacion') border border-danger @enderror" id="" name="fua_observacion" rows="2">{{ old('fua_observacion', !$medical->fua_observacion ? optional($ultimaOrdenNoVacia)->fua_observacion : $medical->fua_observacion) }}</textarea>
                   @error('fua_observacion')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -87,7 +87,7 @@
 
                   <div class="form-group col-sm-12 col-lg-4">
                       <label for="start_pa">PA Inicial</label>
-                      <input type="text" name="start_pa" class="form-control @error('start_pa') border border-danger @enderror" value="{{ !$medical->order->nurse->start_pa ? $medical->start_pa : $medical->order->nurse->start_pa }}">
+                      <input type="text" name="start_pa" class="form-control @error('start_pa') border border-danger @enderror" value="{{ old('start_pa', !$medical->order->nurse->start_pa ? $medical->start_pa : $medical->order->nurse->start_pa) }}">
                       @error('start_pa')
                         <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -95,7 +95,7 @@
 
                   <div class="form-group col-sm-12 col-lg-4">
                       <label for="fc">Frecuencia Cardiaca</label>
-                      <input type="text" name="fc" class="form-control @error('fc') border border-danger @enderror" value="{{ !$medical->order->nurse->fc ? $medical->fc : "" }}">
+                      <input type="text" name="fc" class="form-control @error('fc') border border-danger @enderror" value="{{old('fc', !$medical->order->nurse->fc ? $medical->fc : "") }}">
                       @error('fc')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -103,7 +103,7 @@
 
                   <div class="form-group col-sm-12 col-lg-4">
                       <label for="serology">Serologia</label>
-                      <input type="text" name="serology" class="form-control @error('serology') border border-danger @enderror" value="{{ !$medical->serology ? optional($ultimaOrdenNoVacia)->serology : $medical->serology }}">
+                      <input type="text" name="serology" class="form-control @error('serology') border border-danger @enderror" value="{{old('serology', !$medical->serology ? optional($ultimaOrdenNoVacia)->serology : $medical->serology) }}">
                       @error('serology')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -114,7 +114,7 @@
           <div class="row">
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="hour_hd">HORA HD</label>
-                  <input type="text" name="hour_hd" class="form-control @error('hour_hd') border border-danger @enderror" value="{{ !$medical->hour_hd ? optional($ultimaOrdenNoVacia)->hour_hd : $medical->hour_hd }}">
+                  <input type="text" name="hour_hd" class="form-control @error('hour_hd') border border-danger @enderror" value="{{ old('hour_hd', !$medical->hour_hd ? optional($ultimaOrdenNoVacia)->hour_hd : $medical->hour_hd) }}">
                   @error('hour_hd')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -122,7 +122,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="heparin">Heparina</label>
-                      <input type="text" name="heparin" class="form-control @error('heparin') border border-danger @enderror" value="{{ !$medical->heparin ? optional($ultimaOrdenNoVacia)->heparin : $medical->heparin }}">
+                      <input type="text" name="heparin" class="form-control @error('heparin') border border-danger @enderror" value="{{old('heparin', !$medical->heparin ? optional($ultimaOrdenNoVacia)->heparin : $medical->heparin) }}">
                       @error('heparin')
                         <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -138,7 +138,7 @@
 
               <div class="form-group col-sm-12 col-lg-2">
                   <label for="dry_weight">Peso Seco</label>
-                  <input type="text" name="dry_weight" class="form-control @error('dry_weight') border border-danger @enderror" value="{{ !$medical->dry_weight ? optional($ultimaOrdenNoVacia)->dry_weight : $medical->dry_weight }}">
+                  <input type="text" name="dry_weight" class="form-control @error('dry_weight') border border-danger @enderror" value="{{ old('dry_weight', !$medical->dry_weight ? optional($ultimaOrdenNoVacia)->dry_weight : $medical->dry_weight) }}">
                   @error('dry_weight')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -146,7 +146,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="uf">UF</label>
-                  <input type="text" name="uf" class="form-control @error('uf') border border-danger @enderror" value="{{ !$medical->order->nurse->uf ? $medical->uf : "" }}">
+                  <input type="text" name="uf" class="form-control @error('uf') border border-danger @enderror" value="{{ old('uf', !$medical->order->nurse->uf ? $medical->uf : "") }}">
                   @error('uf')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -154,7 +154,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="qb">QB</label>
-                  <input type="text" name="qb" class="form-control @error('qb') border border-danger @enderror" value="{{ !$medical->qb ? optional($ultimaOrdenNoVacia)->qb : $medical->qb }}">
+                  <input type="text" name="qb" class="form-control @error('qb') border border-danger @enderror" value="{{ old('qb', !$medical->qb ? optional($ultimaOrdenNoVacia)->qb : $medical->qb) }}">
                   @error('qb')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -162,7 +162,7 @@
 
                   <div class="form-group col-sm-12 col-lg-1">
                       <label for="qd">QD</label>
-                      <input type="text" name="qd" class="form-control @error('qd') border border-danger @enderror" value="{{ !$medical->qd ? optional($ultimaOrdenNoVacia)->qd : $medical->qd }}">
+                      <input type="text" name="qd" class="form-control @error('qd') border border-danger @enderror" value="{{ old('qd', !$medical->qd ? optional($ultimaOrdenNoVacia)->qd : $medical->qd) }}">
                       @error('qd')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -170,7 +170,7 @@
 
                   <div class="form-group col-sm-12 col-lg-3">
                       <label for="bicarbonat">Buffer</label>
-                      <input type="text" name="bicarbonat" class="form-control @error('bicarbonat') border border-danger @enderror" value="{{ old("Bicarbonato", !$medical->bicarbonat ? optional($ultimaOrdenNoVacia)->bicarbonat : $medical->bicarbonat) }}">
+                      <input type="text" name="bicarbonat" class="form-control @error('bicarbonat') border border-danger @enderror" value="{{ old("bicarbonat", !$medical->bicarbonat ? optional($ultimaOrdenNoVacia)->bicarbonat : $medical->bicarbonat) }}">
                       @error('bicarbonat')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -182,7 +182,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="cnd">CND</label>
-                  <input type="text" name="cnd" class="form-control @error('cnd') border border-danger @enderror" value="{{ !$medical->cnd ? optional($ultimaOrdenNoVacia)->cnd : $medical->cnd }}">
+                  <input type="text" name="cnd" class="form-control @error('cnd') border border-danger @enderror" value="{{ old('cnd', !$medical->cnd ? optional($ultimaOrdenNoVacia)->cnd : $medical->cnd) }}">
                   @error('cnd')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -190,7 +190,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="start_na">NA INICIAL</label>
-                  <input type="text" name="start_na" class="form-control @error('start_na') border border-danger @enderror" value="{{ !$medical->start_na ? optional($ultimaOrdenNoVacia)->start_na : $medical->start_na }}">
+                  <input type="text" name="start_na" class="form-control @error('start_na') border border-danger @enderror" value="{{ old('start_na', !$medical->start_na ? optional($ultimaOrdenNoVacia)->start_na : $medical->start_na) }}">
                   @error('start_na')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -198,7 +198,7 @@
 
               <div class="form-group col-sm-12 col-lg-1">
                   <label for="end_na">NA FINAL</label>
-                  <input type="text" name="end_na" class="form-control @error('end_na') border border-danger @enderror" value="{{ !$medical->end_na ? optional($ultimaOrdenNoVacia)->end_na : $medical->end_na }}">
+                  <input type="text" name="end_na" class="form-control @error('end_na') border border-danger @enderror" value="{{ old('end_na', !$medical->end_na ? optional($ultimaOrdenNoVacia)->end_na : $medical->end_na) }}">
                   @error('end_na')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -206,7 +206,7 @@
 
                   <div class="form-group col-sm-12 col-lg-1">
                       <label for="profile_na">Perfil Na</label>
-                      <input type="text" name="profile_na" class="form-control @error('profile_na') border border-danger @enderror" value="{{ !$medical->profile_na ? optional($ultimaOrdenNoVacia)->profile_na : $medical->profile_na }}">
+                      <input type="text" name="profile_na" class="form-control @error('profile_na') border border-danger @enderror" value="{{ old('profile_na', !$medical->profile_na ? optional($ultimaOrdenNoVacia)->profile_na : $medical->profile_na) }}">
                       @error('profile_na')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -214,7 +214,7 @@
 
                   <div class="form-group col-sm-12 col-lg-2">
                       <label for="profile_uf">Perfil UF:</label>
-                      <input type="text" name="profile_uf" class="form-control @error('profile_uf') border border-danger @enderror" value="{{ $medical->profile_uf }}">
+                      <input type="text" name="profile_uf" class="form-control @error('profile_uf') border border-danger @enderror" value="{{ old('profile_uf', $medical->profile_uf) }}">
                       @error('profile_uf')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -223,7 +223,7 @@
 
               <div class="form-group col-sm-12 col-lg-3">
                   <label for="area_filter">ÁREA/FILTRO</label>
-                  <input type="text" name="area_filter" class="form-control @error('area_filter') border border-danger @enderror" value="{{ !$medical->area_filter ? optional($ultimaOrdenNoVacia)->area_filter : $medical->area_filter }}">
+                  <input type="text" name="area_filter" class="form-control @error('area_filter') border border-danger @enderror" value="{{ old('area_filter', !$medical->area_filter ? optional($ultimaOrdenNoVacia)->area_filter : $medical->area_filter) }}">
                   @error('area_filter')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
@@ -231,7 +231,7 @@
 
                   <div class="form-group col-sm-12 col-lg-3">
                       <label for="membrane">MEMBRANA</label>
-                      <input type="text" name="membrane" class="form-control @error('membrane') border border-danger @enderror" value="{{ !$medical->membrane ? optional($ultimaOrdenNoVacia)->membrane : $medical->membrane }}">
+                      <input type="text" name="membrane" class="form-control @error('membrane') border border-danger @enderror" value="{{ old('membrane', !$medical->membrane ? optional($ultimaOrdenNoVacia)->membrane : $medical->membrane) }}">
                       @error('membrane')
                       <div class="text-danger text-center text-sm">{{ $message }}</div>
                       @enderror
@@ -244,7 +244,7 @@
 
             <div class="form-group col-sm-12 col-lg-4">
                       <label for="end_evaluation">Evaluación Final</label>
-                      <textarea class="form-control @error('end_evaluation') border border-danger @enderror" id="" name="end_evaluation" rows="2">{{ !$medical->end_evaluation ? optional($ultimaOrdenNoVacia)->end_evaluation : $medical->end_evaluation  }}</textarea>
+                      <textarea class="form-control @error('end_evaluation') border border-danger @enderror" id="" name="end_evaluation" rows="2">{{ old('end_evaluation', !$medical->end_evaluation ? optional($ultimaOrdenNoVacia)->end_evaluation : $medical->end_evaluation) }}</textarea>
                 @error('end_evaluation')
                 <div class="text-danger text-center text-sm">{{ $message }}</div>
                 @enderror
@@ -253,7 +253,7 @@
 
               <div class="form-group col-sm-12 col-lg-2">
                   <label for="end_hour">Hora final</label>
-                  <input type="time" name="end_hour" class="form-control @error('end_hour') border border-danger @enderror" value="{{ !$medical->order->nurse->end_hour ? $medical->end_hour : "" }}">
+                  <input type="time" name="end_hour" class="form-control @error('end_hour') border border-danger @enderror" value="{{ old('end_hour', !$medical->order->nurse->end_hour ? $medical->end_hour : "") }}">
                   @error('end_hour')
                   <div class="text-danger text-center text-sm">{{ $message }}</div>
                   @enderror
